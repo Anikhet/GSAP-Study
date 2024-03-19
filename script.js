902819102eb0212e218e5e20b071ab0f47cab2aa
Anikhet Mulky <am9559@g.rit.edu>
2024-03-19 03:22:09 -0400

    gsap updates

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+var tl = gsap.timeline()
+
+tl.from("#nav h3",{
+    y:-50,
+    delay:0.5,
+    duration:0.6,
+    opacity:0,
+    stagger:0.3
+})
+
+tl.from("#main h1",{
+    x:-500,
+    duration:1,
+    stagger:0.3,
+    opacity:0,
+})
+
+tl.from("img",{
+    x:100,
+    rotate:45,
+    opacity:0,
+    duration:0.5
+})
+
+
+
+
+
+
+
 // gsap.to("#box",{
 //     x:1450,
 //     duration:2,
