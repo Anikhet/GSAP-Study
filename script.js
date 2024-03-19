98c903e0cc85cce8952f86c0c0be284a20c834cb
Anikhet Mulky <am9559@g.rit.edu>
2024-03-19 02:51:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// gsap.to("#box",{
+//     x:1450,
+//     duration:2,
+//     rotate:360,
+//     delay:1,
+//     backgroundColor:"blue",
+// })
+
+
+
+// gsap.from("#box",{
+//     x:1500,
+//     duration:2,
+//     rotate:360,
+//     delay:1,
+//     backgroundColor:"blue",
+// })
+
+// var tl = gsap.timeline()
+// tl.to('#box1',{
+
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+  
+// })
+
+// tl.to('#box2',{
+
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+    
+// })
+// tl.to('#box3',{
+
+//     x:1200,
+//     rotate:360,
+//     scale:0.5,
+//     duration:2,
+    
+// })
